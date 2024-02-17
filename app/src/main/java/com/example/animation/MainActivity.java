f8cf8bc435036be7da146cf04e5a73d4fa46d54a
chanchaljai <chanchal123jaiswal@gmail.com>
2024-02-17 23:18:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+       // lottiefile android web page open and select file and download.
+       //res rightclick new android resource directory and name raw again new and
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
